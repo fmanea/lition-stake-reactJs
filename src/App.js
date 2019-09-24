699fa147cc73935f88e25707f820bcd81508eb0f
Flavian <maneaflavian@gmail.com>
2019-09-25 01:51:37 +0300

    Add custom slider example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,65 @@ import React from 'react';
 import { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 300,
+  },
+  margin: {
+    height: theme.spacing(3),
+  },
+}));
+
+const marks = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 5
+  },
+  {
+    value: 10
+  },
+  {
+    value: 15
+  },
+  {
+    value: 20,
+    label: '20k',
+  },
+  {
+    value: 40 ,
+    label: '40k',
+  },
+  {
+    value: 60,
+    label: '60k',
+  },
+  {
+    value: 80,
+    label: '80k',
+  },
+  {
+    value: 100,
+    label: '100k',
+  },
+];
+
+function valuetext(value) {
+  return `${value}k`;
+}
+
+function valueLabelFormat(value) {
+  return marks.findIndex(mark => mark.value === value) + 1;
+}
+
 
 class Login extends Component {
 
@@ -17,16 +76,15 @@ class Login extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
-    
+
   }
 
-  result = () => { 
-    if (this.state.isGenesisPhase== true)
-    {
-      return (this.state.stakedTokens * 0.00001 * this.state.txNumber * this.state.txCost / 800 * parseFloat(this.state.nodeCategory)*2.74 );
+  result = () => {
+    if (this.state.isGenesisPhase == true) {
+      return (this.state.stakedTokens * 0.00001 * this.state.txNumber * this.state.txCost / 800 * parseFloat(this.state.nodeCategory) * 2.74);
     }
-    return (this.state.stakedTokens * 0.00001 * this.state.txNumber * this.state.txCost / 800 * parseFloat(this.state.nodeCategory) );
-   };
+    return (this.state.stakedTokens * 0.00001 * this.state.txNumber * this.state.txCost / 800 * parseFloat(this.state.nodeCategory));
+  };
 
   handleChange(evt) {
     // check it out: we get the evt.target.name (which will be either "email" or "password")
@@ -34,85 +92,110 @@ class Login extends Component {
     this.setState({ [evt.target.name]: evt.target.value });
   }
 
-  handleCheckboxChange(evt)
-  {
+  handleCheckboxChange(evt) {
     this.setState({ [evt.target.name]: evt.target.checked });
   }
 
+
   render() {
+    const classes = makeStyles(theme => ({
+      root: {
+        width: 300,
+      },
+      margin: {
+        height: theme.spacing(3),
+      },
+    }));
     return (
-      <form>
-        <FormGroup>
-          <label>Staked Tokens</label>
-          <input type="number" name="stakedTokens" value={this.state.stakedTokens} onChange={this.handleChange} />
-        </FormGroup>
+      <div className={classes.root}>
+        <form>
+          <FormGroup>
+            <label>Staked Tokens</label>
+            <input type="number" name="stakedTokens" value={this.state.stakedTokens} onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <label>Lition Token Price(USD)</label>
-          <input type="number" name="tokenPrice" value={this.state.tokenPrice} onChange={this.handleChange} />
-        </FormGroup> 
+          <FormGroup>
+            <label>Lition Token Price(USD)</label>
+            <input type="number" name="tokenPrice" value={this.state.tokenPrice} onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <label>Average Number of Transactions</label>
-          <input type="number" name="txNumber" value={this.state.txNumber} onChange={this.handleChange} />
-        </FormGroup>
+          <FormGroup>
+            <label>Average Number of Transactions</label>
+            <input type="number" name="txNumber" value={this.state.txNumber} onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <label>Per Transaction Cost(USD)</label>
-          <input type="number" name="txCost" value={this.state.txCost} onChange={this.handleChange} />
-        </FormGroup>
+          <FormGroup>
+            <label>Per Transaction Cost(USD)</label>
+            <input type="number" name="txCost" value={this.state.txCost} onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <Label for="nodeCategorySelect">Node Category</Label>
-          <Input type="select" name="nodeCategory" id="nodeCategorySelect" onChange={this.handleChange} >
-            <option value="1">Node</option>
-            <option value="2">Trust Node</option>
-            <option value="1.2">Lition Pool Staking 1 Month</option>
-            <option value="1.4">Lition Pool Staking 3 Months</option>
-            <option value="1.6">Lition Pool Staking 6 Months</option>
-            <option value="1.8">Lition Pool Staking 12 Months</option>
-          </Input>
-        </FormGroup>
+          <FormGroup>
+            <Label for="nodeCategorySelect">Node Category</Label>
+            <Input type="select" name="nodeCategory" id="nodeCategorySelect" onChange={this.handleChange} >
+              <option value="1">Node</option>
+              <option value="2">Trust Node</option>
+              <option value="1.2">Lition Pool Staking 1 Month</option>
+              <option value="1.4">Lition Pool Staking 3 Months</option>
+              <option value="1.6">Lition Pool Staking 6 Months</option>
+              <option value="1.8">Lition Pool Staking 12 Months</option>
+            </Input>
+          </FormGroup>
 
-        <FormGroup>
-          <label>Genesis Phase Activated</label>
-          <input type="checkbox" name="isGenesisPhase" onChange={this.handleCheckboxChange} />
-        </FormGroup>
+          <FormGroup>
+            <label>Genesis Phase Activated</label>
+            <input type="checkbox" name="isGenesisPhase" onChange={this.handleCheckboxChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <label>My Current Lition Value(USD) =    </label>
-          <label>{(this.state.tokenPrice*this.state.stakedTokens).toFixed(2) + "$"}</label>
-        </FormGroup>
+          <FormGroup>
+            <label>My Current Lition Value(USD) =    </label>
+            <label>{(this.state.tokenPrice * this.state.stakedTokens).toFixed(2) + "$"}</label>
+          </FormGroup>
 
-        <FormGroup>
-          <label>Average Daily Earning(USD) =    </label>
-          <label>{(this.result()).toFixed(2)+"$"}</label>
-        </FormGroup>
+          <FormGroup>
+            <label>Average Daily Earning(USD) =    </label>
+            <label>{(this.result()).toFixed(2) + "$"}</label>
+          </FormGroup>
 
-        <FormGroup>
-          <label>Average Monthly Earning(USD) =    </label>
-          <label>{(this.result() *30).toFixed(2)+"$"}</label>
-        </FormGroup>
+          <FormGroup>
+            <label>Average Monthly Earning(USD) =    </label>
+            <label>{(this.result() * 30).toFixed(2) + "$"}</label>
+          </FormGroup>
 
-        <FormGroup>
-          <label>Average Yearly Earning(USD) =    </label>
-          <label>{(this.result() *365).toFixed(2)+"$"}</label>
-        </FormGroup>
+          <FormGroup>
+            <label>Average Yearly Earning(USD) =    </label>
+            <label>{(this.result() * 365).toFixed(2) + "$"}</label>
+          </FormGroup>
 
-        <FormGroup>
-          <label>Annual Staking Rate =    </label>
-          <label>{(this.result() *365 / this.state.tokenPrice / this.state.stakedTokens * 100 ).toFixed(2)+"%"}</label>
-        </FormGroup>
-        <FormGroup>
-          <label>Contribute 1 lit for this community tool (ETH Wallet) =    </label>
-          <label>0x8e4b7c6aE8EC30cbf7Bb6F0a6DD87AB96e3710eb</label>
-        </FormGroup>
-        <FormGroup>
-          <label>GitHub Link for improvements =    </label>
-          <label>https://github.com/fmanea/lition-stake-reactJs.git</label>
-        </FormGroup>
+          <FormGroup>
+            <label>Annual Staking Rate =    </label>
+            <label>{(this.result() * 365 / this.state.tokenPrice / this.state.stakedTokens * 100).toFixed(2) + "%"}</label>
+          </FormGroup>
+          <FormGroup>
+            <label>Contribute 1 lit for this community tool (ETH Wallet) =    </label>
+            <label>0x8e4b7c6aE8EC30cbf7Bb6F0a6DD87AB96e3710eb</label>
+          </FormGroup>
+          <FormGroup>
+            <label>GitHub Link for improvements =    </label>
+            <label>https://github.com/fmanea/lition-stake-reactJs.git</label>
+          </FormGroup>
+        </form>
 
-      </form>
+        <div className={classes.margin} />
+        <Typography id="discrete-slider-small-steps" gutterBottom>
+          Custom marks
+      </Typography>
+        <Slider
+          defaultValue={0}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider-small-steps"
+          step={1}
+          valueLabelDisplay="auto"
+          marks={marks}
+          min={0}
+          max={100}
+        />
+       
+      </div>
     );
   }
 }
@@ -141,6 +224,14 @@ function App() {
   );
 }
 
-
-
 export default App;
+
+
+
+
+
+
+
+
+
+
