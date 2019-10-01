b85ec9c08009365f06c5bd5a08f70131754ba9f6
Flavian <maneaflavian@gmail.com>
2019-10-01 23:12:51 +0300

    Updates

---
+++ precache-manifest.8cb25d587352032bffcc2973511ab33c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa29bfd8c6a9d8c01ec47388ff22197",
+    "url": "/lition-stake-site/index.html"
+  },
+  {
+    "revision": "6ec5be4defbf78ffc44d",
+    "url": "/lition-stake-site/static/css/main.73f3a919.chunk.css"
+  },
+  {
+    "revision": "cab2c7fe7bb4a69ddb2e",
+    "url": "/lition-stake-site/static/js/2.7a7d5c6f.chunk.js"
+  },
+  {
+    "revision": "6ec5be4defbf78ffc44d",
+    "url": "/lition-stake-site/static/js/main.c9f3e895.chunk.js"
+  },
+  {
+    "revision": "b8adaee73e9c454332e0",
+    "url": "/lition-stake-site/static/js/runtime~main.551bfcc8.js"
+  }
+]);
