51b2342d5955d0fcf915f96162640de76583582c
Flavian <maneaflavian@gmail.com>
2019-10-05 18:57:39 +0300

    position switch

--- src/App.js
+++ src/App.js
@@ -182,21 +182,21 @@ class Login extends Component {
             </div>
 
             <div className='textFormPairContainer'>
-              <p>Genesis Phase Activated</p>
+              <label>Activate Genesis Phase</label>
               <Switch
                 name="isGenesisPhase"
                 checked={this.state.isGenesisPhase}
                 onChange={this.handleCheckboxChange}
                 disabled={this.state.genesisPhaseCheckboxState}
                 onColor="#86d3ff"
-                onHandleColor="#2693e6"
-                handleDiameter={30}
+                onHandleColor="#30acc1"
+                handleDiameter={28}
                 uncheckedIcon={false}
                 checkedIcon={false}
                 boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
                 activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-                height={20}
-                width={48}
+                height={15}
+                width={50}
                 className="react-switch"
                 id="material-switch"
               />
